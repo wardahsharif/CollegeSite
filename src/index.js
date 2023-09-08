3ecf60957e6a43a3a8afab613a7c3706e6b4d8fa
wardah <119267583+wardahsharif@users.noreply.github.com>
2023-09-08 12:03:17 +0300

    made some changes

--- src/index.js
+++ src/index.js
@@ -3,16 +3,18 @@ import ReactDOM from 'react-dom/client';
 import './index.css';
 import Main from "./mainSection";
 import Footer from "./Footer";
-
 import Nav from "./Nav.js";
+
+
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
  <div>
+ 
  <Nav/>
   <Main/>
   <Footer/>
- </div>
+   </div>
   </React.StrictMode>
 );
 
